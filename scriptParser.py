78741b403fca37ebfdc7573d692682e3e0287c7e
Aaron Whitehead <aaronbmwhitehead@gmail.com>
2016-02-09 19:10:34 -0800

    scriptParser updated, JSON object created Went through and fixed a bug in the scriptParser that was breaking whenever Chef began singing. Also went ahead and moved the created dict to a JSON object and printed it to file.

--- scriptParser.py
+++ scriptParser.py
@@ -1,5 +1,6 @@
 import time
 import re
+import json
 
 def remove_trash(sent):
 	ret = ''
@@ -23,8 +24,16 @@ def parseEpisode(episodeNumber):
 	count = 2
 	flag = False
 	name = False
+	poem = False
 	currentSpeaker = ""
 	for x in fileObject:
+		if( "<i>[End of" in x or "[<b>End" in x or "</td></tr></table>" in x):
+			break
+		if("poem" in x or poem):
+			poem = True
+			if("</div>" in x):
+				poem = False
+			continue
 		x = remove_trash(x)
 		x = x.replace(".", " ")
 		x = x.replace("?", "")
@@ -32,36 +41,45 @@ def parseEpisode(episodeNumber):
 		x = x.replace(",", "")
 		x = x.replace(";", "")
 		x = x.replace("\"", "")
+		x = x.replace(")", "")
+		x = x.replace("(", "")
 		x = x.upper()
 		wordList = x.split()
 
 		if(wordList == []):
 			continue
 
-		if(wordList == ['&#160']):
-			print "Done parsing this script"
-			break
-
 		#this first part here is just to find the start
 		if(wordList == ['SCRIPT']):
 			count -= 1
 			if(count == 0):
-				print "Found start of script, starting to print!"
 				flag = True
 				continue
 		if(not flag or count != 0):
 			continue
 
 		#means the script has started.
-		time.sleep(0.25)
-		print wordList
+		#time.sleep(0.02)
+		#print wordList
 		if(not name):
 			name = True
-			currentSpeaker = wordList[0]
+			currentSpeaker = x
 			currentSpeaker = currentSpeaker.replace(":", "")
-			break
+			currentSpeaker = currentSpeaker.replace("\n", "")
 		else:
-			#some magic here
+			if(currentSpeaker not in memory):
+				memory[currentSpeaker] = {}
+			for word in wordList:
+				word = re.sub('[^\x00-\x7f]', '', word)
+				if(word not in memory[currentSpeaker]):
+					memory[currentSpeaker][word] = 1
+				else:
+					memory[currentSpeaker][word] += 1
+			name = False
 
-
-parseEpisode(2)
+memory = {}
+for number in range(1, 262):
+	parseEpisode(number)
+#print sorted(((v,k) for k,v in memory["CARTMAN"].iteritems()), reverse=True)
+jsonString = json.dumps(memory)
+print jsonString
